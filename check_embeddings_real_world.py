28c3c393b13c035c87c9ee303627870b25efb278
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2020-02-06 17:15:43 +0000

    real world network embedding progress check

--- check_embeddings_real_world.py
+++ check_embeddings_real_world.py
@@ -19,7 +19,7 @@ def main():
     ):
         embedding_directory = os.path.join(
             "embeddings", "no_feats",
-            "{:02d}".format(dataset), exp, 
+            dataset, exp, 
             "scale={}".format(scale),
             "k={:02d}".format(k),
             "seed={:03d}".format(seed),
