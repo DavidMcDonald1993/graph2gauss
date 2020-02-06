8da7107e25eba1e517a485dcc29c7a3a026edf0e
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2020-02-06 17:14:29 +0000

    real world network embedding progress check

---
+++ check_embeddings_real_world.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import os
+import itertools
+
+from pandas.errors import EmptyDataError
+
+def main():
+
+    datasets = ("cora_ml", "citeseer", "pubmed", "email", "wiki_vote")
+    dims = (2, 5, 10, 25, 50)
+    seeds = range(30)
+    exps = ["recon_experiment", "lp_experiment"]
+    ks = (3, )
+    matrices = ["mu", "sigma"]
+    scales = ("False", )
+
+    for dataset, dim, seed, exp, k, matrix, scale in itertools.product(
+        datasets, dims, seeds, exps, ks, matrices, scales
+    ):
+        embedding_directory = os.path.join(
+            "embeddings", "no_feats",
+            "{:02d}".format(dataset), exp, 
+            "scale={}".format(scale),
+            "k={:02d}".format(k),
+            "seed={:03d}".format(seed),
+            "dim={:03d}".format(dim)
+        )
+
+        filename = os.path.join(embedding_directory, 
+            "{}.csv.gz".format(matrix))
+
+        try:
+            pd.read_csv(filename)
+        except EmptyDataError:
+            print (filename, "is empty removing it")
+            os.remove(filename)
+        except IOError:
+            print (filename, "does not exist")
+
+
+
+
+if __name__ == "__main__":
+    main()
